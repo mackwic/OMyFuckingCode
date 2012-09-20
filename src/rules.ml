1fa8747bbea898ff30cc94538434fa7c9b966812
Thomas Wickham <thomas@endymion.epimeros.org>
2012-09-20 20:19:56 +0200

    completion rules.ml

--- src/rules.ml
+++ src/rules.ml
@@ -15,7 +15,7 @@
  *      + Example: col80_c for the 80-width rule applied for all lines
  *      + or upcaseMacroCheck_pd for a rule checking that all the preprocessor
  *      + macro are upcased
- * 
+ *
  * - set the priority
  *      + priority is used to order the differents rules
  *      + priority is an int between -1000 and 1000, 0 if it doesn't matter
@@ -49,28 +49,60 @@ object(this)
         this#parse '\n'
     | c -> cur_char <- cur_char + 1; this#parse c
 
+  val mutable keeped = BatRope.empty
+
+  method keep c = keeped <- BatRope.append keeped c
+  method release = Some (BatRope.shift keeped)
+
   method virtual parse: (char option) -> (char option)
   method virtual name: string
   method virtual priority: int
+  method virtual print_err:unit -> unit
 
   method bind (obj:rule) = binded <-
     fun c -> obj#parse (binded c)
+
   method line = self#cur_line
 end
 
-(* maybe you need the whole line... *)
-class line_rule =
+(* maybe you need only few chars of the beginning of the line... *)
+class start_line_rule =
 object(self) inherit rule
-  val mutable line = BatRope.empty
-  
-  method parse = function
-    | None -> None
-    | Some c when c = '\n' -> self#parse_line (BatRope.to_string line)
-    | Some c -> line <- BatRope.append_char (UChar.of_char c) line; None
+  val len = List.length chars
+  val virtual chars: char list
 
-  method virtual parse_line: string -> char option
+  val mutable wait = true
+
+  method skip_line c = if c != '\n' then Some c else wait <- false; Some '\n'
+
+  method parse c = function
+          | None -> None
+          | Some c -> if wait then skip_line c else apply c
+
+  method virtual apply: char -> char option
+  >
+
+
+(* maybe you need the whole line... *)
+class line_rule rule =
+object(self) inherit rule
+  val rule = rule
+  val mutable pending = BatRope.empty
+
+  method apply = append '\n';
+          if rule (BatRope.to_string pending) then self#print_err
+          else ();
+          keeped <- BatRope.append keeped pending;
+          pending <- BatRope.empty
+
+  method append c = pending <- BatRope.append pending (UChar.of_char c)
+
+  method parse = function
+          | None -> self#release
+          | Some c when c = '\n' -> self#apply; self#release
+          | Some c -> self#append c; self#release
 >
-  
+
 
 (* /!\ a placer en dernier *)
 let col_80 = <object(self) inherit rule
@@ -84,7 +116,7 @@ let col_80 = <object(self) inherit rule
                          if !count > 80 then error self "Line too large" else ();
                          count := 0;
                          Some '\n')
-                       | Some c -> incr count; c 
+                       | Some c -> incr count; c
           >
 
 let windowsEOL_c = <object(self) inherit rule
@@ -98,34 +130,25 @@ let windowsEOL_c = <object(self) inherit rule
 
 let identity = <object(this) inherit rule
                        method name = "identity_c"
+                       method priority = 0
                        method parse c = c
                               >
 
-let macroCheckName_p = <object(self) inherit rule
-                               method name = "macroCheckName_p"
-                               method priority = 20
-                               method parse c = match c with
-                                 | None -> None
-                                 | Some c -> if not is_preprocess || skip_line
-                                   then match c with
-                                     | '#' -> is_preprocess <- newline; Some '#'
-                                     | '\n' -> newline <- true;
-                                         skip_line <- false;
-                                         Some '\n'
-                                     | c -> newline <- false; Some c
-                                   else (* preprocess instruction + maybe #define *)
-                                     if newline then match c with
-                                       | ' ' -> newline <- false; Some ' '
-                                       | c -> skip_line <-
-                                           not (is_in "define" c cur_char);
-                                           Some c
-                                     else (* after the '#define ' *)
-                                       match c with
-                                     | 'a'..'z' -> error "Name not capitalized";
-                                         Some c
-                                     | ' ' | '(' -> skip_line <- true; Some c
-                                     | c -> Some c
-                                   else match c with
-                                     | 'a'..'z' -> error "Name not capitalized"; c
-                                     | 'A'..'Z' -> is_name <- true; c
-                                     |V
+let macroCheckName_p = <object(self) inherit line_rule (fun l ->
+        Regex.match "^#define .*" l
+        && not (Regex.match "^#define [A-Z][A-Z_]*[ (]"))
+                              method name = "macroCheckName_all"
+                              method priority = 10
+                                    >
+
+let macroCheckArgs_p =
+        let reg1 = Str.regexp "^#define [A-Z][A-Z_]*[ (]"
+        and reg2 = Str.regexp "^#define [A-Z][A-Z_]*[ (]([A-Z][a-z]*,?)+)"
+        in
+        let func = fun line -> (Str.string_match reg1 line 0)
+                                && not (Str.string_match reg2 line 0)
+        in
+        <object(self) inherit line_rule func
+                method name = "macroCheckArgs_p"
+                method priority = 5
+                        >
